19d2da2d39bfb9202559213726c948f9c2c36e13
HJK <han.junkwang@gmail.com>
2020-12-04 18:14:51 +0800

    Day 01 refactoring

--- lib/advent_of_code/day01.ex
+++ lib/advent_of_code/day01.ex
@@ -2,25 +2,22 @@ defmodule AdventOfCode.Day01 do
   @moduledoc "Day 01"
 
   defp pairs_to(numbers, target) do
-    Enum.find(numbers, fn x -> (target - x) in numbers end)
+    Enum.find(numbers, &((target - &1) in numbers))
   end
 
   def part1(numbers) do
-    pairs_to(numbers, 2020)
-    |> (fn x -> x * (2020 - x) end).()
+    (&(&1 * (2020 - &1))).(pairs_to(numbers, 2020))
   end
 
   def part2(numbers) do
-    numbers
-    |> Enum.map(
-         fn x ->
-           case pairs_to(numbers, 2020 - x) do
-             nil -> nil
-             r -> {x, r, 2020 - x - r}
-           end
-         end
-       )
-    |> Enum.find(fn v -> v != nil end)
-    |> (fn {a, b, c} -> a * b * c end).()
+    Enum.find_value(
+      numbers,
+      fn x ->
+        case pairs_to(numbers, 2020 - x) do
+          nil -> nil
+          r -> x * r * (2020 - x - r)
+        end
+      end
+    )
   end
 end
