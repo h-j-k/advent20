847bbe629cdab1e5698715a74f7a13b71c700d1a
HJK <han.junkwang@gmail.com>
2020-12-04 18:34:57 +0800

    Day 03 refactoring

--- lib/advent_of_code/day03.ex
+++ lib/advent_of_code/day03.ex
@@ -2,13 +2,12 @@ defmodule AdventOfCode.Day03 do
   @moduledoc "Day 03"
 
   defp process(list, {right, down}) do
-    list
-    |> Enum.take_every(down)
-    |> Enum.count(
-         fn {line, index} ->
-           String.at(line, rem(div(index, down) * right, String.length(line))) == "#"
-         end
-       )
+    Enum.count(
+      Enum.take_every(list, down),
+      fn {line, index} ->
+        String.at(line, rem(div(index, down) * right, String.length(line))) == "#"
+      end
+    )
   end
 
   def part1(list) do
@@ -18,6 +17,6 @@ defmodule AdventOfCode.Day03 do
   def part2(list) do
     [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
     |> Enum.map(fn move -> process(list, move) end)
-    |> Enum.reduce(fn trees, total -> trees * total end)
+    |> Enum.reduce(&(&1 * &2))
   end
 end
