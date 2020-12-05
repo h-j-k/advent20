307dcb00fd6f5cd5b9576e943bc109c479fbb09a
HJK <han.junkwang@gmail.com>
2020-12-05 17:59:05 +0800

    Day 05 refactoring

--- lib/advent_of_code/day05.ex
+++ lib/advent_of_code/day05.ex
@@ -1,24 +1,11 @@
 defmodule AdventOfCode.Day05 do
   @moduledoc "Day 05"
 
-  @mapping %{"F" => true, "B" => false, "L" => true, "R" => false}
+  @mapping %{"F" => "0", "B" => "1", "L" => "0", "R" => "1"}
 
-  defp partition(is_lower, {min, max}) do
-    (&(if is_lower, do: {min, &1}, else: {&1, max})).(min + div(1 + max - min, 2))
-  end
+  defp tr(value), do: Enum.join(Enum.map(String.graphemes(value), &(@mapping[&1])))
 
-  defp process(range, sequence), do: String.graphemes(sequence)
-                                     |> Enum.map(&(@mapping[&1]))
-                                     |> Enum.reduce(range, &partition/2)
-                                     |> elem(0)
-
-  defp process(list), do: Enum.map(
-    list,
-    fn value ->
-      [_, row, column] = Regex.run(~r/([FB]{7})([LR]{3})/, value)
-      (&(&1 * 8 + &2)).(process({0, 127}, row), process({0, 7}, column))
-    end
-  )
+  defp process(list), do: Enum.map(list, &(String.to_integer(tr(&1), 2)))
 
   def part1(list), do: Enum.max(process(list))
 
