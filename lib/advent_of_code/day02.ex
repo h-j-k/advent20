7d2ee197eb5f097a700970f6c8cec3190910e359
HJK <han.junkwang@gmail.com>
2020-12-06 16:06:22 +0800

    Day 02 refactoring

--- lib/advent_of_code/day02.ex
+++ lib/advent_of_code/day02.ex
@@ -3,16 +3,14 @@ defmodule AdventOfCode.Day02 do
 
   import String
 
-  defp is_valid(list, check) do
-    to_values = &(Enum.drop(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1), 1))
-    checker = fn [p1, p2, char, password] -> check.(to_integer(p1), to_integer(p2), char, password) end
-    Enum.count(list, &(checker.(to_values.(&1))))
-  end
+  defp process(list, is_valid), do: Enum.count(
+    Enum.map(list, &(Enum.drop(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1), 1))),
+    fn [a, b, char, value] -> is_valid.(to_integer(a), to_integer(b), char, value) end
+  )
 
-  def part1(list), do: is_valid(list, &(AdventOfCode.count_chars(&4)[&3] in &1..&2))
+  defp is_char_at(value, char), do: &(at(value, &1 - 1) == char)
 
-  def part2(list) do
-    is_char_at = fn char, password -> &(at(password, &1 - 1) == char) end
-    is_valid(list, &((is_char_at.(&3, &4)).(&1) != (is_char_at.(&3, &4)).(&2)))
-  end
+  def part1(list), do: process(list, &(AdventOfCode.count_chars(&4)[&3] in &1..&2))
+
+  def part2(list), do: process(list, &(is_char_at(&4, &3).(&1) != is_char_at(&4, &3).(&2)))
 end
