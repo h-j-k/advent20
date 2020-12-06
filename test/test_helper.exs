f9fd39db5c9ddb74e0dac3dc662279b642279912
HJK <han.junkwang@gmail.com>
2020-12-06 20:18:33 +0800

    More test code refactoring

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,16 +1,13 @@
 defmodule AdventUtils do
 
-  defmacro test_file(opts \\ []), do: to_list(to_name(Atom.to_string(__CALLER__.module)), opts)
-
-  defp to_name(module), do: Regex.replace(~r/^.*Day(\d\d)Test/, module, "./inputs/day\\1.txt")
-
-  defp to_list(path, opts) do
+  defmacro test_file(opts \\ []) do
+    to_name = &(Regex.replace(~r/^.*Day(\d\d)Test/, &1, "./inputs/day\\1.txt"))
     empty_line = &(&1 == "")
     flatten = &(Enum.join(&1, " "))
     line_delimited = fn s -> Stream.reject(Stream.chunk_by(s, empty_line), &(Enum.all?(&1, empty_line))) end
     process_if = fn (s, opt, process) -> if Keyword.get(opts, opt, false), do: process.(s), else: s end
     map_if = fn (s, opt, mapper) -> process_if.(s, opt, &(Stream.map(&1, mapper))) end
-    File.stream!(path)
+    File.stream!(to_name.(Atom.to_string(__CALLER__.module)))
     |> Stream.map(&String.trim/1)
     |> process_if.(:line_delimited, line_delimited)
     |> process_if.(:flattened, &(Stream.map(line_delimited.(&1), flatten)))
