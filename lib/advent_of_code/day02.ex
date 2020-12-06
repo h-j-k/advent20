3c0dc694eef6493efacd1453920f09a3dbd40bd1
HJK <han.junkwang@gmail.com>
2020-12-06 15:34:21 +0800

    Day 02 refactoring

--- lib/advent_of_code/day02.ex
+++ lib/advent_of_code/day02.ex
@@ -4,21 +4,15 @@ defmodule AdventOfCode.Day02 do
   import String
 
   defp is_valid(list, check) do
-    to_tuple = &(List.to_tuple(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1)))
-    to_props = fn {_, p1, p2, char, password} -> {to_integer(p1), to_integer(p2), char, password} end
-    Enum.count(list, &(check.(to_props.(to_tuple.(&1)))))
+    to_tuple = &(Enum.drop(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1), 1))
+    to_props = fn [p1, p2, char, password] -> check.(to_integer(p1), to_integer(p2), char, password) end
+    Enum.count(list, &(to_props.(to_tuple.(&1))))
   end
 
-  def part1(list) do
-    check = fn {min, max, char, password} -> Enum.frequencies(graphemes(password))[char] in min..max end
-    is_valid(list, check)
-  end
+  def part1(list), do: is_valid(list, &(AdventOfCode.count_chars(&4)[&3] in &1..&2))
 
   def part2(list) do
-    check = fn {p1, p2, char, password} ->
-      is_char_at = &(at(password, &1 - 1) == char)
-      is_char_at.(p1) != is_char_at.(p2)
-    end
-    is_valid(list, check)
+    is_char_at = fn char, password -> &(at(password, &1 - 1) == char) end
+    is_valid(list, &((is_char_at.(&3, &4)).(&1) != (is_char_at.(&3, &4)).(&2)))
   end
 end
