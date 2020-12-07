23d82dfc6f5342ab704e7dff09513f1f59fd3963
HJK <han.junkwang@gmail.com>
2020-12-07 11:27:56 +0800

    Day 02 refactoring

--- lib/advent_of_code/day02.ex
+++ lib/advent_of_code/day02.ex
@@ -1,16 +1,14 @@
 defmodule AdventOfCode.Day02 do
   @moduledoc "Day 02"
 
-  import String
-
   defp process(list, is_valid), do: Enum.count(
-    Enum.map(list, &(Enum.drop(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1), 1))),
-    fn [a, b, char, value] -> is_valid.(to_integer(a), to_integer(b), char, value) end
+    Enum.map(list, &(Regex.run(~r/^(\d+)\-(\d+) (.): (.+)$/, &1))),
+    fn [_, a, b, char, value] -> is_valid.(String.to_integer(a), String.to_integer(b), char, value) end
   )
 
-  defp is_char_at(value, char), do: &(at(value, &1 - 1) == char)
+  defp is_char_at(value, index, char), do: String.at(value, index - 1) == char
 
   def part1(list), do: process(list, &(AdventOfCode.count_chars(&4)[&3] in &1..&2))
 
-  def part2(list), do: process(list, &(is_char_at(&4, &3).(&1) != is_char_at(&4, &3).(&2)))
+  def part2(list), do: process(list, &(is_char_at(&4, &1, &3) != is_char_at(&4, &2, &3)))
 end
