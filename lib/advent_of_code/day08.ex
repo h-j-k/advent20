478c6041aae9abc7b6cbcea543bb6fee1c054d2b
HJK <han.junkwang@gmail.com>
2020-12-08 14:15:59 +0800

    Day 08 refactoring

--- lib/advent_of_code/day08.ex
+++ lib/advent_of_code/day08.ex
@@ -1,34 +1,26 @@
 defmodule AdventOfCode.Day08 do
   @moduledoc "Day 08"
 
-  defp parse(line) do
+  defp parse(line, target, default) do
     [_, instr, offset] = Regex.run(~r/^(acc|jmp|nop) \+?([0-9-]+)$/, line)
-    {instr, String.to_integer(offset)}
+    if instr == target, do: String.to_integer(offset), else: default
   end
 
-  defp process(acc, line) do
-    {instr, offset} = parse(line)
-    if (instr == "acc"), do: acc + offset, else: acc
-  end
-
-  def part1(list) do
-    Stream.unfold(
-      Enum.at(list, 0),
-      fn {line, index} ->
-        {instr, offset} = parse(line)
-        {{line, index}, Enum.at(list, index + (if (instr == "jmp"), do: offset, else: 1))}
-      end
-    )
+  defp generate(list) do
+    process = fn acc, line -> acc + parse(line, "acc", 0) end
+    Enum.at(list, 0)
+    |> Stream.unfold(fn {line, index} -> {{line, index}, Enum.at(list, index + parse(line, "jmp", 1))} end)
     |> Enum.reduce_while(
          {0, []},
          fn {line, index}, {acc, seen} ->
            if (Enum.member?(seen, index)),
               do: {:halt, {acc, seen}},
-              else: {:cont, {process(acc, line), [index | seen]}}
+              else: {:cont, {process.(acc, line), [index | seen]}}
          end
        )
-    |> elem(0)
   end
+
+  def part1(list), do: elem(generate(list), 0)
 
   def part2(_list), do: -1
 end
