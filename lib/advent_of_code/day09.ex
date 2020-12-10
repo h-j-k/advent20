49c3ce73c1bcb6cfa53f915222c5d831d3b1351c
HJK <han.junkwang@gmail.com>
2020-12-10 08:12:26 +0800

    Day 09 refactoring

--- lib/advent_of_code/day09.ex
+++ lib/advent_of_code/day09.ex
@@ -1,17 +1,17 @@
 defmodule AdventOfCode.Day09 do
   @moduledoc "Day 09"
 
-  defp process(list, n \\ 25), do: Enum.find_value(
+  defp process(list, n \\ 25), do: Enum.find(
     Enum.drop(Enum.with_index(list), n),
-    fn {x, index} -> if AdventOfCode.Day01.pairs_to(Enum.slice(list, index - n, n), x) == nil, do: x, else: nil end
+    fn {x, index} -> AdventOfCode.Day01.pairs_to(Enum.slice(list, index - n, n), x) == nil end
   )
 
-  def part1(list), do: process(list)
+  def part1(list), do: elem(process(list), 0)
 
   def part2(list) do
-    target = process(list)
+    {target, start} = process(list)
     Enum.find_value(
-      0..(Enum.count(list) - 1),
+      start..0,
       fn index ->
         result = Enum.reduce_while(
           Enum.reverse(Enum.take(list, index)),
