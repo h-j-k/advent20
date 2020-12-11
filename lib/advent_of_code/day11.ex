bacd73ad0747c8232218af2d3844a726eaee9f7f
HJK <han.junkwang@gmail.com>
2020-12-11 16:35:32 +0800

    Day 11 refactoring

--- lib/advent_of_code/day11.ex
+++ lib/advent_of_code/day11.ex
@@ -15,40 +15,44 @@ defmodule AdventOfCode.Day11 do
     {seats, %Grid{max_x: Enum.count(String.graphemes(elem(hd(list), 0))) - 1, max_y: Enum.count(list) - 1}}
   end
 
-  defp occupied_neighbors(grid, seat), do: Enum.filter(
-    Enum.map(
-      [{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}],
-      fn {x, y} -> %Seat{x: seat.x + x, y: seat.y + y, empty?: false} end
-    ),
-    &(&1.x <= grid.max_x && &1.y <= grid.max_y)
-  )
-
-  defp process(seats, grid) do
+  defp process(seats, grid, get_occupied, to_empty_threshold) do
     fn seat ->
-      occupied = Enum.count(occupied_neighbors(grid, seat), &(MapSet.member?(seats, &1)))
+      occupied = Enum.count(get_occupied.(seats, grid, seat), &(MapSet.member?(seats, &1)))
       empty? = cond do
         seat.empty? && occupied == 0 -> false
-        !seat.empty? && occupied >= 4 -> true
+        !seat.empty? && occupied >= to_empty_threshold -> true
         true -> seat.empty?
       end
       %Seat{x: seat.x, y: seat.y, empty?: empty?}
     end
   end
 
-  def part1(list) do
+  defp process(list, get_occupied, to_empty_threshold) do
     {seats, grid} = parse(list)
+    process = &(MapSet.new(Enum.map(&1, process(&1, grid, get_occupied, to_empty_threshold))))
     r = Enum.at(
       Stream.unfold(
         MapSet.new(seats),
-        fn current ->
-          next = MapSet.new(Enum.map(current, process(current, grid)))
-          if (next == current), do: nil, else: {current, next}
-        end
+        fn current -> (&(if &1 == current, do: nil, else: {current, &1})).(process.(current)) end
       ),
       -1
     )
-    Enum.count(Enum.map(r, process(r, grid)), &(!&1.empty?))
+    Enum.count(process.(r), &(!&1.empty?))
   end
+
+  def part1(list), do: process(
+    list,
+    fn _, grid, seat ->
+      Enum.filter(
+        Enum.map(
+          [{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}],
+          fn {x, y} -> %Seat{x: seat.x + x, y: seat.y + y, empty?: false} end
+        ),
+        &(&1.x <= grid.max_x && &1.y <= grid.max_y)
+      )
+    end,
+    4
+  )
 
   def part2(list) do
     -1
