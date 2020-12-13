19861e2626d8dff832f05e19d59abd66273e5a99
HJK <han.junkwang@gmail.com>
2020-12-14 00:05:07 +0800

    Day 11 refactoring

--- lib/advent_of_code/day11.ex
+++ lib/advent_of_code/day11.ex
@@ -4,15 +4,58 @@ defmodule AdventOfCode.Day11 do
   defmodule Seat, do: defstruct x: 0, y: 0, empty?: true
 
   defmodule SeatMap do
-    defstruct seats: MapSet.new(), row: %{}, col: %{}, same: MapSet.new(), to_process: MapSet.new()
+    defstruct seats: MapSet.new(), row: %{}, neighbors: %{}
 
-    def new(seats, previous \\ MapSet.new()), do: %SeatMap{
-      seats: seats,
-      row: Map.new(Enum.group_by(seats, &(&1.y)), fn {k, v} -> {k, Enum.sort_by(v, &(&1.x))} end),
-      col: Map.new(Enum.group_by(seats, &(&1.x)), fn {k, v} -> {k, Enum.sort_by(v, &(&1.y))} end),
-      same: MapSet.intersection(seats, previous),
-      to_process: MapSet.difference(seats, previous)
-    }
+    defp get_neighbors(seats, by_row) do
+      by_col = Map.new(
+        Enum.group_by(seats, &(&1.x)),
+        fn {k, v} -> {k, Enum.sort_by(v, &(&1.y))} end
+      )
+      neighbors = fn line, finder ->
+        case Enum.find_index(line, finder) do
+          0 -> [Enum.at(line, 1)]
+          n -> Enum.map([-1, 1], &(Enum.at(line, n + &1)))
+        end
+      end
+      Map.new(
+        seats,
+        fn seat ->
+          horizontal = neighbors.(by_row[seat.y], &(&1.x == seat.x))
+          vertical = neighbors.(by_col[seat.x], &(&1.y == seat.y))
+          mapper = fn positions ->
+            {{seat.x, seat.y}, Enum.map(Enum.filter(positions, &(&1 != nil)), fn s -> {s.x, s.y} end)}
+          end
+          Enum.filter(seats, &(abs(seat.x - &1.x) - abs(seat.y - &1.y) == 0))
+          |> Enum.sort_by(&(abs(seat.x - &1.x)))
+          |> Enum.reduce_while(
+               [nil, nil, nil, nil],
+               fn s, [nw, ne, se, sw] ->
+                 updated = cond do
+                   s.x < seat.x && s.y < seat.y && nw == nil -> [s, ne, se, sw]
+                   s.x > seat.x && s.y < seat.y && ne == nil -> [nw, s, se, sw]
+                   s.x > seat.x && s.y > seat.y && se == nil -> [nw, ne, s, sw]
+                   s.x < seat.x && s.y > seat.y && sw == nil -> [nw, ne, se, s]
+                   true -> [nw, ne, se, sw]
+                 end
+                 {(if Enum.any?(updated, &(&1 == nil)), do: :cont, else: :halt), updated}
+               end
+             )
+          |> (fn diagonals -> mapper.(diagonals ++ horizontal ++ vertical) end).()
+        end
+      )
+    end
+
+    def new(seats, neighbors \\ nil) do
+      by_row = Map.new(
+        Enum.group_by(seats, &(&1.y)),
+        fn {k, v} -> {k, Enum.sort_by(v, &(&1.x))} end
+      )
+      %SeatMap{
+        seats: seats,
+        row: by_row,
+        neighbors: (if neighbors == nil, do: get_neighbors(seats, by_row), else: neighbors)
+      }
+    end
   end
 
   defp parse(list) do
@@ -34,12 +77,11 @@ defmodule AdventOfCode.Day11 do
     %Seat{x: seat.x, y: seat.y, empty?: is_empty.(seat_map, seat)}
   end
 
-  defp process(list, is_empty) do
-    process = &(SeatMap.new(MapSet.union(&1.same, MapSet.new(&1.to_process, run(&1, is_empty))), &1.seats))
-    # process = &(SeatMap.new(MapSet.new(&1.seats, run(&1, is_empty))))
+  defp process(list, is_empty, neighbors \\ nil) do
+    process = &(SeatMap.new(MapSet.new(&1.seats, run(&1, is_empty)), &1.neighbors))
     r = Enum.at(
       Stream.unfold(
-        SeatMap.new(parse(list)),
+        SeatMap.new(parse(list), neighbors),
         fn current ->
           next = process.(current)
           if next == current, do: nil, else: {current, next}
@@ -67,53 +109,27 @@ defmodule AdventOfCode.Day11 do
           end
         end
       )
-      if (seat.empty? && occupied == 0) || (!seat.empty? && occupied >= 4),
-         do: !seat.empty?, else: seat.empty?
-    end
+      case occupied do
+        0 when seat.empty? -> false
+        n when n >= 4 and not seat.empty? -> true
+        _ -> seat.empty?
+      end
+    end,
+    %{}
   )
 
   def part2(list) do
-    neighbors = fn line, finder ->
-      case Enum.find_index(line, finder) do
-        0 -> [Enum.at(line, 1)]
-        n -> Enum.map([-1, 1], &(Enum.at(line, n + &1)))
-      end
-    end
-    diagonals = fn seat_map, seat ->
-      Enum.filter(seat_map.seats, &(abs(seat.x - &1.x) - abs(seat.y - &1.y) == 0))
-      |> Enum.sort_by(&(abs(seat.x - &1.x)))
-      |> Enum.reduce_while(
-           [nil, nil, nil, nil],
-           fn s, [nw, ne, se, sw] ->
-             updated = cond do
-               s.x < seat.x && s.y < seat.y && nw == nil -> [s, ne, se, sw]
-               s.x > seat.x && s.y < seat.y && ne == nil -> [nw, s, se, sw]
-               s.x > seat.x && s.y > seat.y && se == nil -> [nw, ne, s, sw]
-               s.x < seat.x && s.y > seat.y && sw == nil -> [nw, ne, se, s]
-               true -> [nw, ne, se, sw]
-             end
-             {(if Enum.any?(updated, &(&1 == nil)), do: :cont, else: :halt), updated}
-           end
-         )
-      |> occupied
-    end
     process(
       list,
       fn seat_map, seat ->
-        horizontal = neighbors.(seat_map.row[seat.y], &(&1.x == seat.x))
-        if seat.empty? && occupied(horizontal) > 0 do
-          seat.empty?
-        else
-          occupied = occupied(horizontal ++ neighbors.(seat_map.col[seat.x], &(&1.y == seat.y)))
-          if (seat.empty? && occupied > 0) || (!seat.empty? && occupied == 0) do
-            seat.empty?
-          else
-            case occupied + diagonals.(seat_map, seat) do
-              0 when seat.empty? -> false
-              n when n >= 5 and not seat.empty? -> true
-              _ -> seat.empty?
-            end
-          end
+        neighbors = Enum.map(
+          seat_map.neighbors[{seat.x, seat.y}],
+          fn {x, y} -> Enum.find(Map.get(seat_map.row, y, []), &(&1.x == x)) end
+        )
+        case occupied(neighbors) do
+          0 when seat.empty? -> false
+          n when n >= 5 and not seat.empty? -> true
+          _ -> seat.empty?
         end
       end
     )
