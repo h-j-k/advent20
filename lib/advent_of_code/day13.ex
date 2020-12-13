4369de32ce84220685c0fb4dd6c9347929f670bd
HJK <han.junkwang@gmail.com>
2020-12-13 22:44:53 +0800

    Day 13 refactoring

--- lib/advent_of_code/day13.ex
+++ lib/advent_of_code/day13.ex
@@ -1,24 +1,23 @@
 defmodule AdventOfCode.Day13 do
   @moduledoc "Day 13"
 
-  def part1(list) do
-    target = String.to_integer(Enum.at(list, 0))
-    String.split(Enum.at(list, 1), ",")
+  def part1([timestamp, buses]) do
+    target = String.to_integer(timestamp)
+    String.split(buses, ",")
     |> Enum.map(&(if &1 == "x", do: target, else: String.to_integer(&1)))
     |> Enum.max_by(&(rem(target, &1)))
     |> (&(&1 * (&1 - rem(target, &1)))).()
   end
 
   defp process({bus, index}, {t, step}) do
-    if Integer.mod(t + index, bus) == 0,
-       do: {t, lcm(step, bus)},
+    if rem(t + index, bus) == 0,
+       do: {t, (&(div(&1 * &2, Integer.gcd(&1, &2)))).(bus, step)},
        else: process({bus, index}, {t + step, step})
   end
 
-  defp lcm(a, b), do: div(a * b, Integer.gcd(a, b))
-
-  def part2(list) do
-    Enum.with_index(String.split(Enum.at(list, 1), ","))
+  def part2([_, buses]) do
+    String.split(buses, ",")
+    |> Enum.with_index
     |> Enum.filter(fn {v, _} -> v != "x" end)
     |> Enum.map(fn {v, index} -> {String.to_integer(v), index} end)
     |> Enum.reduce({0, 1}, &process/2)
