f146d73ca2cc9614b546e9edf95b014d78b9841b
HJK <han.junkwang@gmail.com>
2020-12-15 11:10:10 +0800

    Day 14 refactoring

--- lib/advent_of_code/day14.ex
+++ lib/advent_of_code/day14.ex
@@ -3,50 +3,48 @@ defmodule AdventOfCode.Day14 do
 
   defp convert(value), do: String.graphemes(String.pad_leading(Integer.to_string(value, 2), 36, "0"))
 
-  defp part_one_process(key, value, mask) do
-    mapper = fn
-      {_, "1"} -> "1"
-      {_, "0"} -> "0"
-      {v, "X"} -> v
-    end
-    Enum.zip(convert(value), mask)
-    |> Enum.map(mapper)
-    |> Enum.join
-    |> (&(%{key => String.to_integer(&1, 2)})).()
-  end
-
-  def part_two_process(key, value, mask) do
-    mapper = fn
-      {_, "1"} -> ["1"]
-      {v, "0"} -> [v]
-      {_, "X"} -> ["0", "1"]
-    end
-    Enum.zip(convert(key), mask)
-    |> Enum.map(mapper)
-    |> Enum.reverse
-    |> Enum.reduce(fn x, acc -> Enum.flat_map(acc, fn v -> Enum.map(x, &("#{&1}#{v}")) end) end)
-    |> Enum.map(&(String.to_integer(&1, 2)))
-    |> Map.new(&({&1, value}))
-  end
-
-  defp process(program, processor) do
-    {r, _} = Enum.reduce(
+  defp process(program, processor), do: Enum.reduce(
+    Enum.reduce(
       program,
-      {%{}, ""},
-      fn line, {mem, mask} ->
-        inputs = Regex.named_captures(~r/^(mask = (?<mask>.*)|mem\[(?<k>\d+)\] = (?<v>\d+))$/, line)
-        case inputs["mask"] do
-          "" ->
-            {Map.merge(mem, processor.(String.to_integer(inputs["k"]), String.to_integer(inputs["v"]), mask)), mask}
-          mask ->
-            {mem, String.graphemes(mask)}
+      %{mask: ""},
+      fn line, mem ->
+        case Regex.named_captures(~r/^(mask = (?<mask>.*)|mem\[(?<k>\d+)\] = (?<v>\d+))$/, line) do
+          %{"mask" => "", "k" => k, "v" => v} ->
+            Map.merge(mem, processor.(String.to_integer(k), String.to_integer(v), mem.mask))
+          %{"mask" => mask, "k" => _, "v" => _} ->
+            %{mem | mask: String.graphemes(mask)}
         end
       end
-    )
-    Enum.reduce(r, 0, &(elem(&1, 1) + &2))
-  end
+    ),
+    0,
+    fn {_, v}, acc -> if is_integer(v), do: acc + v, else: acc end
+  )
 
-  def part1(program), do: process(program, &part_one_process/3)
+  def part1(program), do: process(
+    program,
+    fn key, value, mask ->
+      mapper = fn
+        {_, "1"} -> "1"
+        {_, "0"} -> "0"
+        {v, "X"} -> v
+      end
+      Enum.map(Enum.zip(convert(value), mask), mapper)
+      |> (fn masked -> [Enum.join(masked)] end).()
+      |> Map.new(&({key, String.to_integer(&1, 2)}))
+    end
+  )
 
-  def part2(program), do: process(program, &part_two_process/3)
+  def part2(program), do: process(
+    program,
+    fn key, value, mask ->
+      mapper = fn
+        {_, "1"} -> ["1"]
+        {v, "0"} -> [v]
+        {_, "X"} -> ["0", "1"]
+      end
+      Enum.map(Enum.zip(convert(key), mask), mapper)
+      |> Enum.reduce(fn x, acc -> Enum.flat_map(acc, fn v -> Enum.map(x, &(v <> &1)) end) end)
+      |> Map.new(&({String.to_integer(&1, 2), value}))
+    end
+  )
 end
