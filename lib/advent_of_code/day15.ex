39f6a3be3c3a9a7311ea09e23e0201f758064705
HJK <han.junkwang@gmail.com>
2020-12-15 15:54:34 +0800

    Day 15 part 2

--- lib/advent_of_code/day15.ex
+++ lib/advent_of_code/day15.ex
@@ -1,40 +1,21 @@
 defmodule AdventOfCode.Day15 do
   @moduledoc "Day 15"
 
-  defp example(program) do
-    case program do
-      "0,3,6" -> 436
-      "1,3,2" -> 1
-      "2,1,3" -> 10
-      "1,2,3" -> 27
-      "2,3,1" -> 78
-      "3,2,1" -> 438
-      "3,1,2" -> 1836
-    end
+  defp process(input, limit) do
+    base = Map.new(Enum.with_index(String.split(hd(input), ","), 1), fn {n, t} -> {String.to_integer(n), [t]} end)
+    (1 + Enum.count(base))..limit
+    |> Enum.reduce(
+         {base, nil},
+         fn turn, {seen, last} ->
+           past = Enum.take(Map.get(seen, last, []), 2)
+           next = if Enum.count(past) < 2, do: 0, else: Enum.reduce(past, &(&2 - &1))
+           {Map.update(seen, next, [turn], fn [h | rest] -> [turn, h] end), next}
+         end
+       )
+    |> elem(1)
   end
 
-  def part1(input) do
-    numbers = Enum.map(String.split(Enum.at(input, 0), ","), &String.to_integer/1)
-    start = Map.new(Enum.with_index(numbers, 1), &({elem(&1, 0), [elem(&1, 1)]}))
-    {_, r} = Enum.reduce(
-      1..2020,
-      {start, nil},
-      fn turn, {mem, last} ->
-        if turn <= Enum.count(numbers) do
-          {Map.put(mem, Enum.at(numbers, turn - 1), [turn]), Enum.at(numbers, turn - 1)}
-        else
-          past = Map.get(mem, last, [])
-          if Enum.count(past) <= 1 do
-            {Map.put(mem, 0, [turn | Map.get(mem, 0, [])]), 0}
-          else
-            [a, b] = Enum.take(past, 2)
-            {Map.put(mem, a - b, [turn | Map.get(mem, a - b, [])]), a - b}
-          end
-        end
-      end
-    )
-    r
-  end
+  def part1(input), do: process(input, 2020)
 
-  def part2(program), do: 0
+  def part2(input), do: process(input, 30000000)
 end
--- test/day15_test.exs
+++ test/day15_test.exs
@@ -7,17 +7,5 @@ defmodule AdventOfCode.Day15Test do
 
   test "part1", do: assert part1(@input) == 273
 
-  test "part2", do: assert part2(@input) == 0
-
-  @example %{
-    "0,3,6" => 436,
-    "1,3,2" => 1,
-    "2,1,3" => 10,
-    "1,2,3" => 27,
-    "2,3,1" => 78,
-    "3,2,1" => 438,
-    "3,1,2" => 1836
-  }
-
-  test "example", do: Enum.each(@example, fn {given, expected} -> assert part1([given]) == expected end)
+  test "part2", do: assert part2(@input) == 47205
 end
