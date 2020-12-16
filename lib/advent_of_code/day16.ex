2586a3c260faa8916b802b2347848d4d68b34b94
HJK <han.junkwang@gmail.com>
2020-12-16 15:37:56 +0800

    Day 16 part 2

--- lib/advent_of_code/day16.ex
+++ lib/advent_of_code/day16.ex
@@ -15,15 +15,43 @@ defmodule AdventOfCode.Day16 do
 
   defp parse(input) do
     [rules, own, nearby] = input
-    [parse_rules(rules), convert(own), convert(nearby)]
+    {parse_rules(rules), hd(convert(own)), convert(nearby)}
+  end
+
+  defp process(input) do
+    {rules, own, nearby} = parse(input)
+    Enum.reduce(
+      nearby,
+      {nil, nil, []},
+      fn ticket, {_, _, tickets} ->
+        invalid = Enum.reject(ticket, &(Enum.any?(rules, fn {_, checker} -> checker.(&1) end)))
+        {rules, own, [{ticket, invalid} | tickets]}
+      end
+    )
   end
 
   def part1(input) do
-    [rules, _, nearby] = parse(input)
-    nearby
-    |> Enum.flat_map(fn ticket -> Enum.reject(ticket, &(Enum.any?(rules, fn {_, checker} -> checker.(&1) end))) end)
-    |> Enum.sum
+    {_, _, nearby} = process(input)
+    Enum.sum(Enum.flat_map(nearby, &(elem(&1, 1))))
   end
 
-  def part2(_input), do: 0
+  def part2(input) do
+    {rules, own, nearby} = process(input)
+    Enum.filter(nearby, &(elem(&1, 1) == []))
+    |> Enum.map(fn {ticket, _} -> Map.new(Enum.with_index(ticket), fn {n, index} -> {index, [n]} end) end)
+    |> Enum.reduce(fn x, acc -> Map.merge(acc, x, fn _, v1, v2 -> v1 ++ v2 end) end)
+    |> Enum.map(
+         fn {col, values} ->
+           matched = Enum.filter(rules, fn {_, checker} -> Enum.all?(values, &(checker.(&1))) end)
+           {col, Enum.map(matched, &(elem(&1, 0)))}
+         end
+       )
+    |> Enum.sort_by(&(length(elem(&1, 1))))
+    |> Enum.reduce(
+         Map.new(),
+         fn {col, matches}, acc -> Map.put(acc, hd(matches -- Map.keys(acc)), col) end
+       )
+    |> Enum.filter(fn {key, _} -> String.starts_with?(key, "departure") end)
+    |> Enum.reduce(1, fn {key, col}, acc -> Enum.at(own, col) * acc end)
+  end
 end
--- test/day16_test.exs
+++ test/day16_test.exs
@@ -7,5 +7,5 @@ defmodule AdventOfCode.Day16Test do
 
   test "part1", do: assert part1(@input) == 26026
 
-  test "part2", do: assert part2(@input) == 0
+  test "part2", do: assert part2(@input) == 1305243193339
 end
