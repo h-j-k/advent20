a5cfe2a0d950728a56af58b25f6418c2e8627744
HJK <han.junkwang@gmail.com>
2020-12-18 01:14:58 +0800

    Day 17 refactoring

--- lib/advent_of_code/day17.ex
+++ lib/advent_of_code/day17.ex
@@ -18,27 +18,23 @@ defmodule AdventOfCode.Day17 do
     &(&1 != cube && Enum.all?(Enum.zip(Tuple.to_list(&1), Tuple.to_list(cube)), fn {a, b} -> abs(a - b) <= 1 end))
   )
 
-  defp expand(mapper), do: fn acc -> Enum.flat_map(-1..1, &(mapper.([&1 | acc]))) end
+  defp expansion(n), do: fn point ->
+    Enum.reduce(
+      1..n,
+      &([List.to_tuple(Enum.map(Enum.zip(Tuple.to_list(point), &1), fn {p, d} -> p + d end))]),
+      fn _, mapper -> &(Enum.flat_map(-1..1, fn d -> mapper.([d | &1]) end)) end
+    ).([])
+  end
 
-  defp expand(d, mapper), do: Enum.reduce(1..d, mapper, fn _, acc -> expand(acc) end).([])
-
-  defp process(cubes, expansion), do: MapSet.new(
-    Enum.filter(flattened_set(cubes, expansion), &(!(&1 in cubes) && nearby(cubes, &1) == 3))
-    ++ Enum.filter(cubes, &(nearby(cubes, &1) in 2..3))
+  defp cycle(space, n), do: MapSet.new(
+    Enum.filter(flattened_set(space, expansion(n)), &(!(&1 in space) && nearby(space, &1) == 3))
+    ++ Enum.filter(space, &(nearby(space, &1) in 2..3))
   )
 
-  defp process(input, to_cube, expansion), do:
-    Enum.count(Enum.reduce(1..6, parse(input, to_cube), fn _, last -> process(last, expansion) end))
+  defp process(input, to_cube, n), do:
+    Enum.count(Enum.reduce(1..6, parse(input, to_cube), fn _, last -> cycle(last, n) end))
 
-  def part1(input), do: process(
-    input,
-    &({&1, &2, 0}),
-    fn {x0, y0, z0} -> expand(3, fn [z, y, x] -> [{x0 + x, y0 + y, z0 + z}] end) end
-  )
+  def part1(input), do: process(input, &({&1, &2, 0}), 3)
 
-  def part2(input), do: process(
-    input,
-    &({&1, &2, 0, 0}),
-    fn {x0, y0, z0, w0} -> expand(4, fn [w, z, y, x] -> [{x0 + x, y0 + y, z0 + z, w0 + w}] end) end
-  )
+  def part2(input), do: process(input, &({&1, &2, 0, 0}), 4)
 end
