fe5e963b1a8e0cff0acf2a8ce03317b57190c0b5
HJK <han.junkwang@gmail.com>
2020-12-18 01:36:28 +0800

    Day 17 refactoring

--- lib/advent_of_code/day17.ex
+++ lib/advent_of_code/day17.ex
@@ -13,16 +13,13 @@ defmodule AdventOfCode.Day17 do
     end
   )
 
-  defp nearby(space, cube), do: Enum.count(
-    space,
-    &(&1 != cube && Enum.all?(Enum.zip(Tuple.to_list(&1), Tuple.to_list(cube)), fn {a, b} -> abs(a - b) <= 1 end))
-  )
+  defp nearby(space, cube), do:
+    Enum.count(space, &(&1 != cube && Enum.all?(Enum.zip(&1, cube), fn {a, b} -> abs(a - b) <= 1 end)))
 
   defp expand(), do: fn point ->
-    list = Tuple.to_list(point)
     Enum.reduce(
-      1..length(list),
-      &([List.to_tuple(Enum.map(Enum.zip(list, &1), fn {p, d} -> p + d end))]),
+      1..length(point),
+      &([Enum.map(Enum.zip(point, &1), fn {p, d} -> p + d end)]),
       fn _, mapper -> &(Enum.flat_map(-1..1, fn d -> mapper.([d | &1]) end)) end
     ).([])
   end
@@ -34,7 +31,7 @@ defmodule AdventOfCode.Day17 do
 
   defp process(start), do: Enum.count(Enum.reduce(1..6, start, fn _, space -> cycle(space) end))
 
-  def part1(input), do: process(parse(input, &({&1, &2, 0})))
+  def part1(input), do: process(parse(input, &([&1, &2, 0])))
 
-  def part2(input), do: process(parse(input, &({&1, &2, 0, 0})))
+  def part2(input), do: process(parse(input, &([&1, &2, 0, 0])))
 end
