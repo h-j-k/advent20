02c7fd7f60c9fd62d9055cd058b71f9151bb316f
HJK <han.junkwang@gmail.com>
2020-12-18 15:44:12 +0800

    Day 18 refactoring

--- lib/advent_of_code/day18.ex
+++ lib/advent_of_code/day18.ex
@@ -4,33 +4,29 @@ defmodule AdventOfCode.Day18 do
   defmodule Step do
     defstruct x: nil, op: nil
 
-    def take(step, token) when is_integer(token), do:
-      (if step.x == nil, do: [Map.merge(step, %{x: token})], else: [%Step{x: step.op.(step.x, token)}])
-
-    def take(step, "+"), do: [%{step | op: &(&1 + &2)}]
-    def take(step, "*"), do: [%{step | op: &(&1 * &2)}]
-    def take(step, "("), do: [step, %Step{}]
-    def take(step, ")"), do: [step]
+    def take(%Step{x: nil, op: _}, token) when is_integer(token), do: %Step{x: token}
+    def take(%Step{x: x, op: op}, token) when is_integer(token), do: %Step{x: op.(x, token)}
+    def take(step, "+"), do: %{step | op: &+/2}
+    def take(step, "*"), do: %{step | op: &*/2}
     def take(%Step{x: _, op: nil}, last) when is_struct(last), do: last
     def take(%Step{x: x, op: op}, last) when is_struct(last), do: %Step{x: op.(x, last.x)}
   end
 
-  defp parse(line), do: hd(
-    Enum.reduce(
-      String.graphemes(String.replace(line, " ", "")),
-      [%Step{}],
-      fn v, [step | rest] ->
-        token = (if Regex.match?(~r/^\d+$/, v), do: String.to_integer(v), else: v)
-        Enum.reduce(
-          Step.take(step, token),
-          rest,
-          fn x, acc -> if token == ")", do: [Step.take(hd(acc), x) | tl(acc)], else: [x | acc] end
-        )
+  defp parse(line), do: Enum.reduce(
+    String.graphemes(String.replace(line, " ", "")),
+    [%Step{}],
+    fn v, [step | rest] ->
+      case (if Regex.match?(~r/^\d+$/, v), do: String.to_integer(v), else: v) do
+        "(" -> [%Step{}, step | rest]
+        ")" -> [Step.take(hd(rest), step) | tl(rest)]
+        token -> [Step.take(step, token) | rest]
       end
-    )
-  ).x
+    end
+  )
 
-  def part1(input), do: Enum.sum(Enum.map(input, &parse/1))
+  defp process(line), do: hd(parse(line)).x
+
+  def part1(input), do: Enum.sum(Enum.map(input, &process/1))
 
   def part2(_input), do: 0
 end
