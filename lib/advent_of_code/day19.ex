070c40f1089fe7f05050a35b0433e4453a9ee3ba
HJK <han.junkwang@gmail.com>
2020-12-19 22:36:40 +0800

    Day 19 refactoring

--- lib/advent_of_code/day19.ex
+++ lib/advent_of_code/day19.ex
@@ -4,34 +4,37 @@ defmodule AdventOfCode.Day19 do
   @is_char ~r/^"(.)"$/
 
   defp get(rules, indices) when is_list(indices), do: Enum.reduce(
-    Enum.map(indices, fn index -> get(rules, index) end),
+    Enum.map(indices, &(get(rules, &1))),
     fn x, acc -> Enum.flat_map(acc, fn v -> Enum.map(x, &(v <> &1)) end) end
   )
 
-  defp get(rules, index) when is_integer(index), do: Enum.flat_map(
-    rules[index],
-    fn
-      v when is_binary(v) -> [v]
-      x -> get(rules, x)
-    end
-  )
+  defp get(rules, index) when is_integer(index), do:
+    MapSet.new(Enum.flat_map(rules[index], &(if is_binary(&1), do: [&1], else: get(rules, &1))))
 
-  def part1(input) do
-    convert = fn x, delimiter, mapper -> Enum.map(String.split(x, delimiter), mapper) end
-    rules = Map.new(
-      hd(input),
+  defp convert(x, delimiter, mapper), do: Enum.map(String.split(x, delimiter), mapper)
+
+  defp parse(rules, overrides), do: Map.merge(
+    Map.new(
+      rules,
       fn line ->
         [_, key, rule] = Regex.run(~r/^(\d+): (.*)$/, line)
         value = cond do
           Regex.match?(@is_char, rule) -> (&(tl(&1))).(Regex.run(@is_char, rule))
-          true -> convert.(rule, " | ", fn x -> convert.(x, " ", &String.to_integer/1) end)
+          true -> convert(rule, " | ", fn x -> convert(x, " ", &String.to_integer/1) end)
         end
         {String.to_integer(key), value}
       end
-    )
-    matches = MapSet.new(get(rules, 0))
+    ),
+    overrides
+  )
+
+  defp process(input, overrides \\ %{}) do
+    rules = parse(hd(input), overrides)
+    matches = get(rules, 0)
     Enum.count(Enum.at(input, 1), &(&1 in matches))
   end
+
+  def part1(input), do: process(input)
 
   def part2(input), do: 0
 end
