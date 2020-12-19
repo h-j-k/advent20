3b5e53a46273f53268d15d7e67b359d91aed2f29
HJK <han.junkwang@gmail.com>
2020-12-19 22:56:18 +0800

    Day 19 refactoring

--- lib/advent_of_code/day19.ex
+++ lib/advent_of_code/day19.ex
@@ -13,28 +13,31 @@ defmodule AdventOfCode.Day19 do
 
   defp convert(x, delimiter, mapper), do: Enum.map(String.split(x, delimiter), mapper)
 
-  defp parse(rules, overrides), do: Map.merge(
-    Map.new(
-      rules,
-      fn line ->
-        [_, key, rule] = Regex.run(~r/^(\d+): (.*)$/, line)
-        value = cond do
-          Regex.match?(@is_char, rule) -> (&(tl(&1))).(Regex.run(@is_char, rule))
-          true -> convert(rule, " | ", fn x -> convert(x, " ", &String.to_integer/1) end)
-        end
-        {String.to_integer(key), value}
+  defp parse(rules), do: Map.new(
+    rules,
+    fn line ->
+      [_, key, rule] = Regex.run(~r/^(\d+): (.*)$/, line)
+      value = cond do
+        Regex.match?(@is_char, rule) -> (&(tl(&1))).(Regex.run(@is_char, rule))
+        true -> convert(rule, " | ", fn x -> convert(x, " ", &String.to_integer/1) end)
       end
-    ),
-    overrides
+      {String.to_integer(key), value}
+    end
   )
 
-  defp process(input, overrides \\ %{}) do
-    rules = parse(hd(input), overrides)
+  defp process(input) do
+    rules = parse(hd(input))
     matches = get(rules, 0)
-    Enum.count(Enum.at(input, 1), &(&1 in matches))
+    Enum.group_by(Enum.at(input, 1), &(&1 in matches))
   end
 
-  def part1(input), do: process(input)
+  def part1(input), do: Enum.count(process(input)[true])
 
-  def part2(input), do: 0
+  def part2(input) do
+    rules = parse(hd(input))
+    by_0 = process(input)
+    original = Enum.count(by_0[true])
+    leftovers = by_0[false]
+    0
+  end
 end
