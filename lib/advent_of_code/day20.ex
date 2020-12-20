f2b648c4b29cebbad301fdcec7d23117a6605f97
HJK <han.junkwang@gmail.com>
2020-12-20 15:16:36 +0800

    Day 20 refactoring

--- lib/advent_of_code/day20.ex
+++ lib/advent_of_code/day20.ex
@@ -16,16 +16,24 @@ defmodule AdventOfCode.Day20 do
     {String.to_integer(id), sides}
   end
 
-  def part1(input) do
+  defp process_tiles(input) do
     tiles = Enum.map(input, &parse_tile/1)
-    corners = Enum.filter(
+    Map.new(
       tiles,
       fn {id, sides} ->
         aligns? = fn {tile_id, tile_sides} -> tile_id != id && Enum.any?(sides, &(&1 in tile_sides)) end
-        Enum.count(tiles, aligns?) == 2
+        {{id, sides}, Enum.filter(tiles, aligns?)}
       end
     )
-    Enum.reduce(corners, 1, fn {id, _}, acc -> id * acc end)
+  end
+
+  def part1(input) do
+    tiles = process_tiles(input)
+    Enum.reduce(
+      Enum.filter(tiles, fn {_, matches} -> Enum.count(matches) == 2 end),
+      1,
+      fn {{id, _}, _}, acc -> id * acc end
+    )
   end
 
   def part2(input) do
