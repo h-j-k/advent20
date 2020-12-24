550a9ea88bb130ee3e4c15c4ba729a12d5f2bea4
HJK <han.junkwang@gmail.com>
2020-12-24 21:16:01 +0800

    Day 24 part 2

--- lib/advent_of_code/day24.ex
+++ lib/advent_of_code/day24.ex
@@ -13,12 +13,34 @@ defmodule AdventOfCode.Day24 do
 
   defp shift(moves), do: Enum.reduce(moves, {0, 0, 0}, &offset/2)
 
-  def part1(input), do: Enum.count(
-    Enum.reduce(input, %{}, fn line, map -> Map.update(map, shift(parse(line)), true, &(!&1)) end),
-    fn {_, black?} -> black? end
+  defp map_tiles(input), do:
+    Enum.reduce(input, %{}, fn line, map -> Map.update(map, shift(parse(line)), true, &(!&1)) end)
+
+  def part1(input), do: Enum.count(map_tiles(input), fn {_, black?} -> black? end)
+
+  def black?(active, size), do: fn tile ->
+    zipped = &(Enum.zip(Tuple.to_list(&1), Tuple.to_list(tile)))
+    Enum.count(active, &(&1 != tile && Enum.all?(zipped.(&1), fn {a, b} -> abs(a - b) <= 1 end))) in size
+  end
+
+  @neighbors [:e, :w, :nw, :ne, :sw, :se]
+
+  defp cycle(tiles, 0), do: Enum.count(tiles)
+
+  defp cycle(tiles, i), do: cycle(
+    MapSet.new(
+      Enum.filter(
+        MapSet.new(Enum.flat_map(tiles, fn tile -> Enum.map(@neighbors, &(offset(&1, tile))) end)),
+        black?(tiles, [2])
+      )
+      ++ Enum.filter(tiles, black?(tiles, 1..2))
+    ),
+    i - 1
   )
 
-  def part2(input) do
-    0
-  end
+  def part2(input), do:
+    map_tiles(input)
+    |> Enum.reduce([], fn {tile, black?}, acc -> if black?, do: [tile | acc], else: acc end)
+    |> MapSet.new
+    |> cycle(100)
 end
--- test/day24_test.exs
+++ test/day24_test.exs
@@ -1,5 +1,5 @@
 defmodule AdventOfCode.Day24Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import AdventOfCode.{Day24, TestUtils}
 
@@ -7,5 +7,6 @@ defmodule AdventOfCode.Day24Test do
 
   test "part1", do: assert part1(@input) == 465
 
-  test "part2", do: assert part2(@input) == 0
+  @tag timeout: :infinity
+  test "part2", do: assert part2(@input) == 4078
 end
