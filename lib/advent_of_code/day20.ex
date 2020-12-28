1c590bf0d799f2d6e4e8ce6113643bcdeaca4e70
HJK <han.junkwang@gmail.com>
2020-12-28 18:29:48 +0800

    Day 20 refactoring, Happy New Year!

--- lib/advent_of_code/day20.ex
+++ lib/advent_of_code/day20.ex
@@ -2,32 +2,30 @@ defmodule AdventOfCode.Day20 do
   @moduledoc "Day 20"
 
   defmodule Tile do
-    defstruct id: nil, content: [], sides: []
+    defstruct id: nil, area: [], sides: []
 
-    def new(id, content) do
+    def new(id, area) do
       sides = [
-        hd(content),
-        Enum.join(Enum.map(content, &String.first/1)),
-        Enum.join(Enum.map(content, &String.last/1)),
-        Enum.at(content, -1)
+        hd(area),
+        Enum.join(Enum.map(area, &String.first/1)),
+        Enum.join(Enum.map(area, &String.last/1)),
+        Enum.at(area, -1)
       ]
-      %Tile{id: id, content: content, sides: Enum.flat_map(sides, &([&1, String.reverse(&1)]))}
+      %Tile{id: id, area: area, sides: Enum.flat_map(sides, &([&1, String.reverse(&1)]))}
     end
 
     def align?(tile, other), do: tile.id != other.id && Enum.any?(tile.sides, &(&1 in other.sides))
   end
 
-  defp parse_tile(tile) do
-    [_, id] = Regex.run(~r/^Tile (\d+):$/, hd(tile))
-    Tile.new(String.to_integer(id), tl(tile))
-  end
+  defp parse_tile(tile), do:
+    (fn [_, id] -> Tile.new(String.to_integer(id), tl(tile)) end).(Regex.run(~r/^Tile (\d+):$/, hd(tile)))
 
   defp process_tiles(input) do
     tiles = Enum.map(input, &parse_tile/1)
     Map.new(tiles, fn tile -> {tile, MapSet.new(Enum.filter(tiles, &(Tile.align?(tile, &1))))} end)
   end
 
-  defp to_row(tile, row, size, has_side?, tiles, last_row), do:
+  defp to_row(tile, row, size, has_side?, tiles, last_row), do: Enum.reverse(
     Enum.reduce(
       1..(size - 1),
       [tile],
@@ -48,7 +46,7 @@ defmodule AdventOfCode.Day20 do
         [next, last | rest]
       end
     )
-    |> Enum.reverse
+  )
 
   defp to_grid(input) do
     tiles = process_tiles(input)
@@ -70,83 +68,65 @@ defmodule AdventOfCode.Day20 do
   end
 
   defp options(tile) when is_struct(tile) do
-    flip_ew = fn content -> Enum.map(content, &String.reverse/1) end
-    flip_ns = fn content -> Enum.reverse(content) end
-    rotate = fn content ->
-      size = length(content) - 1
-      Enum.map(0..size, fn x -> Enum.join(Enum.map(size..0, &(String.at(Enum.at(content, &1), x)))) end)
+    flip_ew = fn area -> Enum.map(area, &String.reverse/1) end
+    flip_ns = fn area -> Enum.reverse(area) end
+    rotate = fn area ->
+      size = length(area) - 1
+      Enum.map(0..size, fn x -> Enum.join(Enum.map(size..0, &(String.at(Enum.at(area, &1), x)))) end)
     end
-    content = tile.content
+    area = tile.area
     [
-      content,
-      flip_ew.(content),
-      flip_ns.(content),
-      rotate.(content),
-      flip_ew.(rotate.(content)),
-      flip_ns.(rotate.(content)),
-      rotate.(rotate.(content)),
-      rotate.(rotate.(rotate.(content)))
+      area,
+      flip_ew.(area),
+      flip_ns.(area),
+      rotate.(area),
+      flip_ew.(rotate.(area)),
+      flip_ns.(rotate.(area)),
+      rotate.(rotate.(area)),
+      rotate.(rotate.(rotate.(area)))
     ]
   end
 
-  defp options(content) when is_list(content), do: [content, Enum.reverse(content)]
+  defp options(area) when is_list(area), do: [area, Enum.reverse(area)]
 
   defp match_tile(first, second) do
-    right = fn content -> Enum.join(Enum.map(content, &String.last/1)) end
-    left = fn content -> Enum.join(Enum.map(content, &String.first/1)) end
+    right = fn area -> Enum.join(Enum.map(area, &String.last/1)) end
+    left = fn area -> Enum.join(Enum.map(area, &String.first/1)) end
     a = if Enum.count(first) == 1,
            do: hd(first),
            else: Enum.find(first, fn x -> Enum.any?(second, &(right.(x) == left.(&1))) end)
     b = Enum.find(second, &(right.(a) == left.(&1)))
-    Enum.map(0..(length(a) - 1), fn i -> Enum.at(a, i) <> Enum.at(b, i) end)
+    Enum.map(0..(length(a) - 1), fn i -> String.slice(Enum.at(a, i), 0..-2) <> String.slice(Enum.at(b, i), 1..-1) end)
   end
 
   defp match_row(first, second) do
-    bottom = fn content -> Enum.at(content, -1) end
-    top = fn content -> hd(content) end
+    bottom = fn area -> Enum.at(area, -1) end
+    top = fn area -> hd(area) end
     a = if Enum.count(first) == 1,
            do: hd(first),
            else: Enum.find(first, fn x -> Enum.any?(second, &(bottom.(x) == top.(&1))) end)
-    Enum.concat(a, Enum.find(second, &(bottom.(a) == top.(&1))))
+    Enum.concat(Enum.drop(a, -1), Enum.drop(Enum.find(second, &(bottom.(a) == top.(&1))), 1))
   end
 
-  defp to_sea(grid), do: Enum.reduce(
-    Enum.with_index(grid),
-    [],
-    fn {row, y}, sea ->
-      row_content = Enum.reduce(
-        Enum.with_index(row),
-        [],
-        fn
-          {tile, 1}, _ -> match_tile(options(hd(row)), options(tile))
-          {tile, n}, content when n > 1 -> match_tile([content], options(tile))
-          _, content -> content
-        end
-      )
-      case y do
-        1 -> match_row(options(sea), options(row_content))
-        n when n > 1 -> match_row([sea], options(row_content))
-        _ -> row_content
+  defp combine(list, mapper, combiner), do: Enum.reduce(
+    Enum.with_index(list),
+    nil,
+    fn {x, i}, acc ->
+      result = mapper.(x)
+      case i do
+        0 -> result
+        1 -> combiner.(options(acc), options(result))
+        _ -> combiner.([acc], options(result))
       end
     end
   )
 
-  defp remove_borders(line, size) when is_binary(line), do:
-    String.graphemes(line)
-    |> Enum.with_index
-    |> Enum.filter(fn {_, c} -> rem(c, size) != 0 && rem(c + 1, size) != 0 end)
-    |> Enum.map(&(elem(&1, 0)))
-    |> Enum.join
-
-  defp remove_borders(sea, size) when is_list(sea), do:
-    Enum.with_index(sea)
-    |> Enum.reduce(
-         [],
-         fn {line, r}, acc ->
-           if rem(r, size) == 0 || rem(r + 1, size) == 0, do: acc, else: [remove_borders(line, size) | acc]
-         end
-       )
-    |> Enum.reverse
+  defp to_sea(input) do
+    sea = combine(to_grid(input), fn row -> combine(row, &(&1), &match_tile/2) end, &match_row/2)
+          |> Enum.slice(1..-2)
+          |> Enum.map(&(String.slice(&1, 1..-2)))
+    {sea, Enum.reduce(sea, 0, &(Enum.count(String.graphemes(&1), fn x -> x == "#" end) + &2))}
+  end
 
   @shapes %{0 => ~r/^..................#.$/, 1 => ~r/^#....##....##....###$/, 2 => ~r/^.#..#..#..#..#..#...$/}
 
@@ -160,25 +140,21 @@ defmodule AdventOfCode.Day20 do
     end
   )
 
-  defp count_monsters_for(sea), do:
-    0..(length(sea) - 3)
-    |> Enum.reduce(0, &(count_monsters_in(Enum.slice(sea, &1, 3)) + &2))
-    |> (&(if &1 == 0, do: nil, else: &1)).()
-
-  defp count_monsters_all(sea_tile), do:
-    Enum.find_value(options(sea_tile), &(count_monsters_for(&1)))
-
-  defp count_fills(sea), do:
-    Enum.sum(Enum.map(sea, &(Enum.count(String.graphemes(&1), fn x -> x == "#" end))))
+  defp count_monsters(tile), do: Enum.find_value(
+    options(tile),
+    fn sea ->
+      case Enum.reduce(0..(length(sea) - 3), 0, &(count_monsters_in(Enum.slice(sea, &1, 3)) + &2)) do
+        0 -> nil
+        n -> n
+      end
+    end
+  )
 
   def part1(input), do:
     process_tiles(input)
     |> Enum.filter(fn {_, matches} -> Enum.count(matches) == 2 end)
     |> Enum.reduce(1, &(elem(&1, 0).id * &2))
 
-  def part2(input) do
-    grid = to_grid(input)
-    sea = remove_borders(to_sea(grid), length(hd(hd(grid)).content))
-    count_fills(sea) - (count_monsters_all(Tile.new(0, sea)) * 15)
-  end
+  def part2(input), do:
+    (fn {sea, fills} -> fills - (count_monsters(Tile.new(0, sea)) * 15) end).(to_sea(input))
 end
